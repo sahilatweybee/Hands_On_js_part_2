992f608cb9b3ebacd7151fa2ae5a9702b10c3c83
Sahil Parsaniya <sahil@webtheuniverse.com>
2022-06-23 17:55:56 +0530

    Completed assignment-3

--- assignments.js
+++ assignments.js
@@ -1,24 +1,24 @@
 'use strict';
 
 // Lecture: Functions
-function describeCountry(country, population, capitalCity){
+function describeCountry(country, population, capitalCity) {
     let description = `${country} has ${population} million people and its capital city is ${capitalCity}`;
     return description;
 }
 console.log(describeCountry("India", 1400, "Delhi"));
 
 // Lecture: Function Declaation vs.Expressions
-function percentageOfWorld1(population){
+function percentageOfWorld1(population) {
     return population * 100 / 7900;
 }
 
-var India = percentageOfWorld1(1400);
+let India = percentageOfWorld1(1400);
 let Austrailia = percentageOfWorld1(25.7);
 let Finland = percentageOfWorld1(5.53);
 
 console.log(`\n ${India} \t ${Austrailia} \t ${Finland}`);
 
-const percentageOfWorld2 = function(population){
+const percentageOfWorld2 = function (population) {
     return population * 100 / 7900;
 }
 
@@ -29,4 +29,13 @@ let Russia = percentageOfWorld2(141.1);
 console.log(`\n ${UK} \t ${USA} \t ${Russia}`);
 
 //Lecture: Arrow Functions
+const percentageOfWorld3 = population => population * 100 / 7900;
 
+UK = percentageOfWorld3(67.2);
+USA = percentageOfWorld3(329.5);
+Russia = percentageOfWorld3(141.1);
+India = percentageOfWorld3(1400);
+Austrailia = percentageOfWorld3(25.7);
+Finland = percentageOfWorld3(5.53);
+
+console.log(`${India} \t ${Austrailia} \t ${Finland} \n ${UK} \t ${USA} \t ${Russia} \n `);
