51aa952c956d5f73b67d4d623d929b09a9c2cd38
Sahil Parsaniya <sahil@webtheuniverse.com>
2022-06-24 16:21:21 +0530

    completed all the assignments and coding challenges

--- Coding_Challenges.js
+++ Coding_Challenges.js
@@ -1,6 +1,6 @@
 // Coding Challenge-1
 'use strict';
-
+console.log("////////////////////////////////////////////////////\n//////////////////////////////////////////////////////\n")
 const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 function checkWinner(avgDolphins, avgKoalas) {
     if (avgDolphins >= 2 * avgKoalas) {
@@ -72,3 +72,27 @@ else {
 }
 
 // Coding Challenge-4
+const calcTip4 = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+var bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+var tips = [];
+var totals = [];
+  
+for (let i = 0; i < bills.length; i++) {
+    let tip = calcTip4(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(`BILLS: ${bills},\nTIPS: ${tips},\nTOTALS: ${totals}`);
+
+const calcAverage4 = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(`\n${calcAverage4([2, 3, 7])}`);
+console.log(`${calcAverage4(totals)}`);
+console.log(`${calcAverage4(tips)}`);
--- script.js
+++ script.js
@@ -395,7 +395,7 @@ for (let exercise = 1; exercise < 4; exercise++) {
     console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} `);
   }
 }
-
+*/
 
  
 // The while Loop
@@ -416,32 +416,3 @@ while (dice !== 6) {
   dice = Math.trunc(Math.random() * 6) + 1;
   if (dice === 6) console.log('Loop is about to end...');
 }
-*/
-
-/*
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
-
-for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(tip + bills[i]);
-}
-console.log(bills, tips, totals);
-
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    // sum = sum + arr[i];
-    sum += arr[i];
-  }
-  return sum / arr.length;
-}
-console.log(calcAverage([2, 3, 7]));
-console.log(calcAverage(totals));
-console.log(calcAverage(tips));
-*/
